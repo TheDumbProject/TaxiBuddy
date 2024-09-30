a1ccade1d52ffa75131905c65b5e77ff1f43234f
thisisanshrastogi <ansh23bcs151@iiitkottayam.ac.in>
2024-09-30 01:08:44 +0100

    requests and toasts added

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,14 +1,15 @@
 import Navbar from "./componentsReact/Navbar";
 import Footer from "./componentsReact/Footer";
 import App from "./App";
+import { Toaster } from "@/components/ui/toaster";
 import { Outlet } from "react-router-dom";
 
 function Layout() {
   return (
     <>
       <Navbar />
+      <Toaster />
       <Outlet />
-      <App />
     </>
   );
 }
--- src/componentsReact/MyBookings/InitiatedCard.tsx
+++ src/componentsReact/MyBookings/InitiatedCard.tsx
@@ -1,43 +1,62 @@
 import { set } from "date-fns";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { add, format } from "date-fns";
 
-function InitiatedCard({ showInBox, setShowInBox }) {
+function InitiatedCard({ showInBox, setShowInBox, booking }) {
   const [getRequests, setGetRequests] = useState(true);
+
   const showRequests = () => {
     if (getRequests) {
       setGetRequests(false);
     }
   };
+  useEffect(() => {
+    console.log("mounted initiated card");
+    console.log(showInBox);
+    // console.log(format(booking.timebooked, "HH:mm"));
+    // console.log(format(booking.datebooked, "MMMM dd"));
+  }, [showInBox]);
+
   return (
     <div className="bg-black hover:bg-[#131313]  border-[1.3px] border-primary rounded-[.8rem] grid grid-cols-3 py-5 my-4 ">
       <div className="flex justify-center gap-3 items-center">
-        <div className="time text-2xl text-white ">12:00</div>
+        <div className="time text-2xl text-white ">
+          {booking.timebooked.slice(0, 5)}
+        </div>
         <div className="text-2xl pb-1">-</div>
-        <div className="time text-xl text-white ">Aug 28</div>
+        <div className="time text-xl text-white ">
+          {format(booking.datebooked, "MMM dd")}
+        </div>
       </div>
       <div className="flex justify-evenly items-center col-span-2">
         <button
-          onClick={() => setShowInBox("settings")}
+          onClick={() => setShowInBox("settings", booking.bookingid)}
           className=" bg-primary transition ease-in-out delay-50  text-black font-medium rounded-2xl text-md px-6 py-2 rounded-3xl border-[1.5px] hover:border-primary hover:bg-black hover:text-primary  "
         >
           Settings
         </button>
+
         <button
-          onClick={() => setShowInBox("requests")}
-          disabled={showInBox === "requests"}
+          onClick={() => setShowInBox(["requests", booking.bookingid])}
+          disabled={
+            showInBox[0] === "requests" && showInBox[1] == booking.bookingid
+          }
           className={
-            showInBox === "requests"
+            showInBox[0] === "requests" && showInBox[1] == booking.bookingid
               ? "bg-black text-primary font-medium text-md px-6 py-2 rounded-3xl border-[1.5px] border-primary hover:bg-black hover:text-primary"
               : "bg-primary transition delay-25 text-black font-medium  text-md px-6 py-2 rounded-3xl border-[1.5px] hover:border-primary hover:bg-black hover:text-primary"
           }
         >
           Requests
         </button>
+
         <button
-          onClick={() => setShowInBox("buddies")}
-          disabled={showInBox === "buddies"}
+          onClick={() => setShowInBox(["buddies", booking.bookingid])}
+          disabled={
+            showInBox[0] === "buddies" && showInBox[1] == booking.bookingid
+          }
           className={
-            showInBox === "buddies"
+            showInBox[0] === "buddies" && showInBox[1] == booking.bookingid
               ? "bg-black text-primary font-medium  text-md px-6 py-2 rounded-3xl border-[1.5px] border-primary hover:bg-black hover:text-primary"
               : "bg-primary  text-black font-medium  text-md px-6 py-2 rounded-3xl border-[1.5px] hover:border-primary hover:bg-black hover:text-primary"
           }
